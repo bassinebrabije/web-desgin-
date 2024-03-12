b2a523aa442c08fbcdc808de7a434514e54e46d5
brabije yassine <130862839+bassinebrabije@users.noreply.github.com>
2024-03-12 12:51:11 +0100

    Add files via upload

---
+++ tactical/JS/message.js
@@ -0,0 +1,8 @@
+setTimeout(function () {
+  document.getElementById("messageContainer").style.display = "block";
+}, 7300);
+
+// Function to hide the message
+function hideMessage() {
+  document.getElementById("messageContainer").style.display = "none";
+}
---
+++ tactical/JS/script.js
@@ -0,0 +1,9 @@
+function loader() {
+  document.querySelector(".loader").style.display = "none";
+}
+
+function fadeOut() {
+  setInterval(loader, 2000);
+}
+
+window.onload = fadeOut;
